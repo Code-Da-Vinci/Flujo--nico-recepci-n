133ebcb96df4691f5be207a7912d27e65b2b338c
Daniel Rojas <erojashi@isise.edu.pe>
2023-10-29 15:23:05 -0500

    Agregando todo

---
+++ main.js
@@ -0,0 +1,79 @@
+const activeBtn = document.querySelector('#uno')
+const desactBtn = document.querySelector('.cajaA')
+const activBtn2 = document.querySelector('#dos')
+const desactBtn2 = document.querySelector('.cajaB')
+const activBtn3 = document.querySelector('#tres')
+const desactBtn3 = document.querySelector('.cajaC')
+const activBtn4 = document.querySelector('#cuatro')
+const desactBtn4 = document.querySelector('.cajaD')
+const activBtn5 = document.querySelector('#cinco')
+const desactBtn5 = document.querySelector('.cajaE')
+const activBtn6 = document.querySelector('#seis')
+const desactBtn6 = document.querySelector('.cajaF')
+const activBtn7 = document.querySelector('#siete')
+const desactBtn7 = document.querySelector('.cajaG')
+const activBtn8 = document.querySelector('#ocho')
+const desactBtn8 = document.querySelector('.cajaH')
+const activBtn9 = document.querySelector('#nueve')
+const desactBtn9 = document.querySelector('.cajaI')
+const activBtn10 = document.querySelector('#diez')
+const desactBtn10 = document.querySelector('.cajaJ')
+const activBtn11 = document.querySelector('#once')
+const desactBtn11 = document.querySelector('.cajaK')
+const activBtn12 = document.querySelector('#doce')
+const desactBtn12 = document.querySelector('.cajaL')
+const activBtn13 = document.querySelector('#trece')
+const desactBtn13 = document.querySelector('.caja_fin')
+
+
+activeBtn.addEventListener('click',()=>{
+   desactBtn.classList.toggle('activo1')
+})
+
+activBtn2.addEventListener('click',()=>{
+   desactBtn2.classList.toggle('activo2')
+})
+
+activBtn3.addEventListener('click',()=>{
+   desactBtn3.classList.toggle('activo3')
+})
+
+activBtn4.addEventListener('click',()=>{
+   desactBtn4.classList.toggle('activo4')
+})
+
+activBtn5.addEventListener('click',()=>{
+   desactBtn5.classList.toggle('activo5')
+})
+
+activBtn6.addEventListener('click',()=>{
+   desactBtn6.classList.toggle('activo6')
+})
+
+activBtn7.addEventListener('click',()=>{
+   desactBtn7.classList.toggle('activo7')
+})
+
+activBtn8.addEventListener('click',()=>{
+   desactBtn8.classList.toggle('activo8')
+})
+
+activBtn9.addEventListener('click',()=>{
+   desactBtn9.classList.toggle('activo9')
+})
+
+activBtn10.addEventListener('click',()=>{
+   desactBtn10.classList.toggle('activo10')
+})
+
+activBtn11.addEventListener('click',()=>{
+   desactBtn11.classList.toggle('activo11')
+})
+
+activBtn12.addEventListener('click',()=>{
+   desactBtn12.classList.toggle('activo12')
+})
+
+activBtn13.addEventListener('click',()=>{
+   desactBtn13.classList.toggle('activo13')
+})
